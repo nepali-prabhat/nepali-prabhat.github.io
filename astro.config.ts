e8bc92aa897184dc70a45b386657914b382b2df5
Pravat Pandey <pandeypravat@gmail.com>
2024-11-04 22:44:49 +0545

    removed duplicate site

--- astro.config.ts
+++ astro.config.ts
@@ -97,7 +97,6 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://prabhatcharita.com/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
