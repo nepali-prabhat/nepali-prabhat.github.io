529f3692b9d19305af1d0a62240d454ad92dc110
Pravat Pandey <pandeypravat@gmail.com>
2024-11-04 23:04:55 +0545

    used remark-unwrap-images

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,7 @@ import { siteConfig } from "./src/site.config";
 
 // Remark plugins
 import remarkDirective from "remark-directive"; /* Handle ::: directives as nodes */
-import remarkUnwrapImages from "remark-unwrap-images";
+import rehypeUnwrapImages from "rehype-unwrap-images";
 import { remarkAdmonitions } from "./src/plugins/remark-admonitions"; /* Add admonitions */
 import { remarkReadingTime } from "./src/plugins/remark-reading-time";
 
@@ -86,8 +86,9 @@ export default defineConfig({
 					target: "_blank",
 				},
 			],
+      rehypeUnwrapImages,
 		],
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime, remarkDirective, remarkAdmonitions],
+		remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
 		remarkRehype: {
 			footnoteLabelProperties: {
 				className: [""],
