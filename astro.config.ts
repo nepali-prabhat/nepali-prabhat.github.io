90d840203f9b759315c12e1b741270819d92b1b2
Pravat Pandey <pandeypravat@gmail.com>
2024-11-11 18:42:53 +0545

    fixed overflow issue, added bg

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import mdx from "@astrojs/mdx";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
@@ -12,9 +12,7 @@ import { expressiveCodeOptions } from "./src/site.config";
 import { siteConfig } from "./src/site.config";
 
 // Remark plugins
-import remarkDirective from "remark-directive"; /* Handle ::: directives as nodes */
 import rehypeUnwrapImages from "rehype-unwrap-images";
-import { remarkAdmonitions } from "./src/plugins/remark-admonitions"; /* Add admonitions */
 import { remarkReadingTime } from "./src/plugins/remark-reading-time";
 
 // Rehype plugins
@@ -22,104 +20,104 @@ import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  site: 'https://nepali-prabhat.github.io',
+  output: "static",
+  site: "https://nepali-prabhat.github.io",
   // base: 'prabhatcharita',
-	image: {
-		domains: ["webmention.io"],
-	},
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		icon(),
-		tailwind({
-			applyBaseStyles: false,
-			nesting: true,
-		}),
-		sitemap(),
-		mdx(),
+  image: {
+    domains: ["webmention.io"],
+  },
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    sitemap(),
+    mdx(),
     react(),
-		robotsTxt(),
-		webmanifest({
-			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
-			/**
-			 * required
-			 **/
-			name: siteConfig.title,
-			/**
-			 * optional
-			 **/
-			// short_name: "Astro_Cactus",
-			description: siteConfig.description,
-			lang: siteConfig.lang,
-			icons: [
-				{
-					src: "public/icon/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
-					sizes: "180x180",
-					type: "image/png",
-				},
-				{
-					src: "public/icon/android-chrome-192x192.png",
-					sizes: "192x192",
-					type: "image/png",
-				},
-				{
-					src: "public/icon/android-chrome-512x512.png",
-					sizes: "512x512",
-					type: "image/png",
-				},
-			],
-			start_url: "/",
-			background_color: "#ffffff",
-			theme_color: "#ffffff",
-			display: "standalone",
-			config: {
-				insertFaviconLinks: false,
-				insertThemeColorMeta: false,
-				insertManifestLink: false,
-			},
-		}),
-	],
-	markdown: {
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					rel: ["nofollow, noreferrer"],
-					target: "_blank",
-				},
-			],
+    robotsTxt(),
+    webmanifest({
+      // See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
+      /**
+       * required
+       **/
+      name: siteConfig.title,
+      /**
+       * optional
+       **/
+      // short_name: "Astro_Cactus",
+      description: siteConfig.description,
+      lang: siteConfig.lang,
+      icons: [
+        {
+          src: "public/icon/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "public/icon/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "public/icon/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+      start_url: "/",
+      background_color: "#ffffff",
+      theme_color: "#ffffff",
+      display: "standalone",
+      config: {
+        insertFaviconLinks: false,
+        insertThemeColorMeta: false,
+        insertManifestLink: false,
+      },
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["nofollow, noreferrer"],
+          target: "_blank",
+        },
+      ],
       rehypeUnwrapImages,
-		],
-		remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: [""],
-			},
-		},
-	},
-	// https://docs.astro.build/en/guides/prefetch/
-	prefetch: true,
-	// ! Please remember to replace the following site property with your own domain
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-		plugins: [rawFonts([".ttf", ".woff"])],
-	},
+    ],
+    remarkPlugins: [remarkReadingTime],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""],
+      },
+    },
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  // ! Please remember to replace the following site property with your own domain
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+    plugins: [rawFonts([".ttf", ".woff"])],
+  },
 });
 
 function rawFonts(ext: string[]) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// @ts-expect-error:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // @ts-expect-error:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
 }
